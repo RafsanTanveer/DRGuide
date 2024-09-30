2a283f6ce7a5c51e8fd32aad256ecc113ebe790f
rafsantanveer <rzrt.edu@gmail.com>
2024-09-30 12:26:30 +0600

    Link update

--- app/(tabs)/info.jsx
+++ app/(tabs)/info.jsx
@@ -9,7 +9,7 @@ const info = () => {
     <ScrollView style={{ padding: height * .02,  }}>
 
 
-      <Card title1="Generl Info" img1="generalinfo" title2="Diabetes Type" img2="diabatestype" isSecondTileVisible={true} />
+      <Card title1="Generl Info" img1="generalinfo" title2="Diabetes Type" img2="diabatestype" isSecondTileVisible={true} link1={'/infoScreens/generalInfo'} />
       <Card title1="Diabetes Complicatins" img1="diabetescomplication" title2="Diabetes Management" img2="diabetesmanagement" isSecondTileVisible={true} />
       <Card title1="Diabetes Retinopathy" img1="retinopathy" title2="Guidelines" img2="guidelines" isSecondTileVisible={true} />
       <Card title1="Practical" img1="practical" title2="Reference" img2="reference" isSecondTileVisible={true} />
--- components/card.jsx
+++ components/card.jsx
@@ -1,26 +1,39 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Pressable } from 'react-native'
 import React from 'react'
 import { height, width } from '../utility/screenDimensions'
 import { Images } from '../utility/Images'
+import { Link } from 'expo-router';
 
-const card = ({ title1, img1, title2, img2, isSecondTileVisible}) => {
-  return (
-      <View style={{ flexDirection: 'row', margin:10 }} >
-          <TouchableOpacity onPress={() => { }}
-              style={{elevation:10, backgroundColor: '#008080', height: width * .4, width: width * .4, alignItems: 'center', justifyContent: 'center', margin: 10, borderRadius:height*.005 }} >
-              <Image style={{ height: width * .2, width: width * .2 }} source={Images[img1]} />
-              <Text style={{ color: 'white', padding: 5, fontSize: height * .02, fontWeight: '600', textAlign: 'center' }} >{title1}</Text>
-          </TouchableOpacity>
-          {
-              isSecondTileVisible &&
-              <TouchableOpacity onPress={() => { }}
-                      style={{ elevation: 10, backgroundColor: '#008080', height: width * .4, width: width * .4, alignItems: 'center', justifyContent: 'center', margin: 10, borderRadius: height * .005 }} >
-                      <Image style={{ height: width * .15, width: width * .15 }} source={Images[img2]} />
-                      <Text style={{ color: 'white', padding: 5, fontSize: height * .02, fontWeight: '600', textAlign:'center' }} >{title2}</Text>
-              </TouchableOpacity>
-          }
-      </View>
-  )
+
+const card = ({ title1, img1, title2, img2, isSecondTileVisible, link1, link2 }) => {
+
+
+
+
+    return (
+        <View style={{ flexDirection: 'row', margin: 10 }} >
+
+            <TouchableOpacity  onPress={() => { }}
+                 >
+
+                <Link style={{ flexDirection: 'column', elevation: 10, backgroundColor: '#008080', height: width * .4, width: width * .4, alignItems: 'center', justifyContent: 'center', margin: 10, borderRadius: height * .005 }} href={`${link1}`}>
+                        <Image style={{ height: width * .2, width: width * .2 }} source={Images[img1]} />
+                        <Text style={{ color: 'white', padding: 5, fontSize: height * .02, fontWeight: '600', textAlign: 'center' }} >{title1}</Text>
+                    </Link>
+
+            </TouchableOpacity>
+
+
+            {
+                isSecondTileVisible &&
+                <TouchableOpacity onPress={() => { }}
+                    style={{ elevation: 10, backgroundColor: '#008080', height: width * .4, width: width * .4, alignItems: 'center', justifyContent: 'center', margin: 10, borderRadius: height * .005 }} >
+                    <Image style={{ height: width * .15, width: width * .15 }} source={Images[img2]} />
+                    <Text style={{ color: 'white', padding: 5, fontSize: height * .02, fontWeight: '600', textAlign: 'center' }} >{title2}</Text>
+                </TouchableOpacity>
+            }
+        </View>
+    )
 }
 
 export default card
