2c52687437e10f5f1e050622bd9d2b9a4f18bf9b
RafsanTanveer <rzrt.edu@gmail.com>
2024-10-03 16:27:21 +0600

    Update

--- app/customNavigation.jsx
+++ app/customNavigation.jsx
@@ -3,6 +3,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useContext, useEffect, useState } from "react";
+
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { height } from '../utility/screenDimensions'
